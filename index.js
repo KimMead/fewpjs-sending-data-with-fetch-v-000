714f9b212ec76cdd3d3036a41326620d24ea502f
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-30 18:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // Add your code here
-fetch("http://localhost:3000/dogs", {
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
   },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
-  })
+  body: JSON.stringify(formData)
 };
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
